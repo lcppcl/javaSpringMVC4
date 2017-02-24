ccb9c0a68b8474b02aad599f70b3681dd290169a
lcp <https://github.com/lcppcl>
2017-02-24 14:29:23 +0800

    springmvc4

---
+++ SpringMvc04/src/com/java1234/controller/FileUploadController.java
@@ -0,0 +1,32 @@
+package com.java1234.controller;
+
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileUploadController {
+
+	@RequestMapping("/upload")
+	public String uploadFile(@RequestParam("file1") MultipartFile file1,HttpServletRequest request)throws Exception{
+		String filePath=request.getServletContext().getRealPath("/");
+		System.out.println(filePath);
+		file1.transferTo(new File(filePath+"upload/"+file1.getOriginalFilename()));
+		return "redirect:success.html";
+	}
+	
+	@RequestMapping("/upload2")
+	public String uploadFiles(@RequestParam("file") MultipartFile[] files,HttpServletRequest request)throws Exception{
+		String filePath=request.getServletContext().getRealPath("/");
+		System.out.println(filePath);
+		for(MultipartFile file:files){
+			file.transferTo(new File(filePath+"upload/"+file.getOriginalFilename()));			
+		}
+		return "redirect:success.html";
+	}
+}
